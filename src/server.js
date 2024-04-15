a8abb78dfc5ec1f0e007dc4b2a93e0cb007f4d42
sanjuna s prasad <sanjunasprasad@gmail.com>
2024-04-15 09:19:18 +0530

    cors error fixed

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ connectDB();
 const server = http.createServer(app)
 const io = new Server( server,{
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://connectify-cyan.vercel.app/",
     },
   });
 
