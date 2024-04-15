59a7f63142a5ce556fa6763936044e9fc3d0dd57
sanjuna s prasad <sanjunasprasad@gmail.com>
2024-04-15 10:53:08 +0530

    console removed

--- src/interfaces/controllers/messageControllers.js
+++ src/interfaces/controllers/messageControllers.js
@@ -2,14 +2,14 @@ import MessageModel from "../../entities/messageModel.js";
 
 export const addMessage = async (req, res) => {
 
-  console.log("sended++++++++++++")
+  
   const { chatId, senderId, text,    } = req.body;
     // Check if an image was uploaded
     const image = req.file ? req.file.path : null;
-    console.log("image is ", image)
-    console.log("chatid",chatId)
-    console.log("senderid",senderId)
-    console.log("message",text)
+    // console.log("image is ", image)
+    // console.log("chatid",chatId)
+    // console.log("senderid",senderId)
+    // console.log("message",text)
   const message = new MessageModel({
     chatId,
     senderId,
@@ -29,7 +29,7 @@ export const getMessages = async (req, res) => {
   console.log("chatid",chatId)
   try {
     const result = await MessageModel.find({ chatId });
-    console.log("received message is ",result)
+    // console.log("received message is ",result)
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json(error);
