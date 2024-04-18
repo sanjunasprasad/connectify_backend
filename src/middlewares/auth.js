a56a496ebe8f55e79c9b9fefd2d8cfeb6e4d98a0
sanjuna s prasad <sanjunasprasad@gmail.com>
2024-04-18 13:46:44 +0530

    changed22

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -23,7 +23,7 @@ export const generateUserToken = async(existingUser) => {
 export const decodeToken = async(req, res, next) => {
     try {
         let token = null
-        console.log("header",req.headers)
+        console.log("USER HEADER",req.headers)
         const header = req.headers.authorization
         if (header !== undefined ){
              token = header.split(" ")[1]
