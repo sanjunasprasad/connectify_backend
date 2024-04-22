7d155de2b4a62be13a77a54ecfca54ca6d2052d0
sanjuna s prasad <sanjunasprasad@gmail.com>
2024-04-22 16:35:03 +0530

    message

--- src/interfaces/routes/MessageRoute.js
+++ src/interfaces/routes/MessageRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const messageRoute = express.Router();
-// import { upload } from '../../middlewares/multer.js';
-// import { addMessage, getMessages } from '../controllers/messageControllers.js';
+import { upload } from '../../middlewares/multer.js';
+import { addMessage, getMessages } from '../controllers/messageControllers.js';
 import {  getMessages } from '../controllers/messageControllers.js';
 
 
 
-// messageRoute.post('/', upload.single("image"),addMessage)
+messageRoute.post('/', upload.single("image"),addMessage)
 messageRoute.get('/:chatId', getMessages);
 
 
